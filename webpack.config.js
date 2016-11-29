27dc59bf27cbc61f9300cdc9413b78e17c682095
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-11-29 21:56:22 +0100

    change rollup to webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: __dirname + '/src/app.js',
+    output: {
+        path: __dirname + '/dist', 
+        filename: '/bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        // Nice colored output
+        colors: true
+    }
+};
